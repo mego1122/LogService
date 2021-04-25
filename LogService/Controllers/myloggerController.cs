b8a6d47abd1fc4e208ce51605e9d206d9b66d870
Abd ElMageed <mohamed.abdelmageed@atos.net>
2021-04-25 10:34:07 +0200

    Add project files.

---
+++ LogService/Controllers/myloggerController.cs
@@ -0,0 +1,57 @@
+﻿using LogService.LoggerService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class myloggerController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+
+        public myloggerController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        // GET: api/<myloggerController>
+        [HttpPost]
+        public string POST([FromBody] object obj)
+        {
+            _logger.LogInfo("Fetching all responses coming from the backend");
+            var contentType = this.Request.ContentType;
+            _logger.LogInfo($"response {obj} " + "custom message");
+
+            return "value2";
+        }
+
+
+
+
+
+
+        // GET api/<myloggerController>/5
+        [HttpGet("{id}")]
+        public string GET(string id)
+        
+        {
+            var contentType = this.Request.ContentType;
+            var accept = this.Request.Headers;
+            var acceptx = this.Request.Method;
+            _logger.LogInfo("Fetching all request coming from the apigee");
+            _logger.LogInfo($"requests {id} " + "custom message");
+            return "value";
+        }
+
+       
+
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //x=19 ,y=2.2
+            // int name =value;
+            //string name = "mohamed";
+            int x = 19;
+            int y = 5;
+            //int z = x + y;
+
+            //Console.WriteLine(z+name);
+
+            //if (x == 18)
+            //{
+            //    Console.WriteLine("x=19");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no value");
+            //}
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int[] numbers = { 1,2,3,4};
+
+            string[] names = { "mohamed", "ali","ahmed"};
+            Console.WriteLine(names.Length);
+        
+            for (int i = 0; i <names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
